4f000f45d2114a1eb6f5ca5db9a00a441dd97914
Sang O. Kim <sangkim1@D-172-28-35-194.dhcp4.washington.edu>
2015-11-23 04:18:58 -0800

    Completed

--- js/main.js
+++ js/main.js
@@ -25,11 +25,9 @@ myApp.config(function($stateProvider) {
 })
 
 myApp.controller('HomeController', function($scope) {
-    
 })
 
 myApp.controller('AboutController', function($scope) {
-
 })
 
 myApp.controller('ProjectsController', function($scope) {
@@ -44,68 +42,9 @@ myApp.controller('ProjectsController', function($scope) {
 })
 
 myApp.controller('ContactController', function($scope) {
-
 })
 
+// for sidebar menu
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
 });
-
-// var currentIndex = 0,
-//   items = $('.container div'),
-//   itemAmt = items.length;
-
-// function cycleItems() {
-//   var item = $('.container div').eq(currentIndex);
-//   items.hide();
-//   item.css('display','inline-block');
-// }
-
-/*
-
-var myApp = angular.module('myApp', ['ui.router'])
-
-myApp.config(function($stateProvider) {
-    $stateProvider
-        .state('dawg-coffee', {
-            url: '/',
-            templateUrl: 'templates/dawg-coffee.html',
-            controller: 'DawgController'
-        })
-        .state('police-shooting', {
-            url: '/police-shooting',
-            templateUrl: 'templates/police-shooting.html',
-            controller: 'PoliceController'
-        })
-        .state('spotify', {
-            url: '/spotify',
-            templateUrl: 'templates/spotify.html',
-            controller: 'SpotifyController'
-        })
-})
-
-// Landing page controller: define $scope.number as a number
-myApp.controller('DawgController', function($scope) {
-    $scope.number = 1
-    //$scope.url = "http://students.washington.edu/sangkim1/info343/dawg-coffee/"
-})
-
-myApp.controller('PoliceController', function($scope) {
-    $scope.number = 2
-    //$scope.url = "http://students.washington.edu/sangkim1/info343/police-shooting/"
-})
-
-myApp.controller('SpotifyController', function($scope) {
-    $scope.number = 3
-    //$scope.url = "http://students.washington.edu/sangkim1/info343/spotify-template/"
-})
-
-Papa.parse("../proj", {
-    download: true,
-    header: true,
-    complete: function(projects) {
-        console.log(projects);
-    }
-})
-
-*/
